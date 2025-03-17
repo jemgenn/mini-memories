cea8b7ce804cd6f408c1d25759e8d9e1f20126d4
Jem <jem@reddingdesigns.com>
2025-03-17 16:04:14 -0400

    adding pages

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    {{ $slot }}
+</body>
+</html>
